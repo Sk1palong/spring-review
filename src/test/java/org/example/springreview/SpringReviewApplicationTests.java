46fc9dc64ca88c05282b08c4a024eab13bc272e2
Sk1palong <cchs33@gmail.com>
2023-12-12 12:36:38 +0900

    init

---
+++ src/test/java/org/example/springreview/SpringReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
