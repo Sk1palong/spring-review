9fa05749a47707f1bdfc8cb1e045728db8e12397
Sk1palong <cchs33@gmail.com>
2023-12-17 15:32:43 +0900

    feat : 이미지 업로드 기능 추가

--- src/main/java/org/example/springreview/Post/Post.java
+++ src/main/java/org/example/springreview/Post/Post.java
@@ -4,7 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.springreview.Timestamped;
+import org.example.springreview.image.Image;
 import org.example.springreview.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -40,6 +44,9 @@ public class Post extends Timestamped {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @OneToMany(mappedBy = "post")
+    private List<Image> images = new ArrayList<>();
+
     /**
      * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
      */
@@ -48,9 +55,13 @@ public class Post extends Timestamped {
         this.username = user.getUsername();
     }
 
-
-
     /**
      * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
      */
+    public void createImage(Image image) {
+        this.images.add(image);
+        if(!image.getPost().equals(this)) {
+            image.setPost(this);
+        }
+    }
 }
--- src/main/java/org/example/springreview/Post/PostController.java
+++ src/main/java/org/example/springreview/Post/PostController.java
@@ -5,10 +5,13 @@ import org.example.springreview.security.UserDetailsImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,10 +22,10 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/posts")
-    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    @PostMapping(value = "/posts", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE} )
+    public ResponseEntity<PostResponseDto> createPost(@RequestPart PostRequestDto requestDto, @RequestPart("files")MultipartFile[] files, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
 
-        PostResponseDto responseDto = postService.createPost(requestDto, userDetails.getUser());
+        PostResponseDto responseDto = postService.createPost(requestDto, files, userDetails.getUser());
 
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(responseDto);
     }
