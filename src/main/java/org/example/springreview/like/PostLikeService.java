5d38d5d1c354f738e8ffb24710034dba323792f1
Sk1palong <cchs33@gmail.com>
2023-12-18 17:49:45 +0900

    feat : 게시글 좋아요 기능 추가

---
+++ src/main/java/org/example/springreview/like/PostLikeService.java
@@ -0,0 +1,40 @@
+package org.example.springreview.like;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springreview.Post.Post;
+import org.example.springreview.Post.PostRepository;
+import org.example.springreview.exception.CustomException;
+import org.example.springreview.exception.ErrorCode;
+import org.example.springreview.user.User;
+import org.example.springreview.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+
+    private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public void createLike(Long postId, User user) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new CustomException(ErrorCode.POST_NOT_FOUND)
+        );
+
+        if (Objects.equals(user.getId(), post.getUser().getId())) {
+            throw new CustomException(ErrorCode.SELF_LIKE_ERROR);
+        }
+        PostLike postLike = PostLike.builder()
+                .user(user)
+                .post(post)
+                .build();
+        if (postLikeRepository.findByUserAndPost(user, post).isPresent()) {
+            throw new CustomException(ErrorCode.ALREADY_EXIST_LIKE);
+        }
+
+        postLikeRepository.save(postLike);
+    }
+}
